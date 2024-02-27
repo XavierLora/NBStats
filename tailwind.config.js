e74796018f7a16c104edacf6c963149b15aa1638
Xavier Lora <XavierLora0727@gmail.com>
2024-02-26 21:07:11 -0500

    First Major UI push for proof of concept. Next steps are to design UI for live games, stats and Blog. Need to consolidate files better for more DRY code.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/_includes/*.html", "./_site/*.html"],
+  content: ["./src/_includes/*.html", "./_site/*.html", "./_site/**/*.html"],
   theme: {
     extend: {}
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#ffffff",
+          "secondary": "#ff00ff",
+          "accent": "#00ffff",
+          "neutral": "#212124",
+          "base-100": "#161618",
+          "info": "#0000ff",
+          "success": "#00ff00",
+          "warning": "#00ff00",
+          "error": "#ff0000",
+        },
+        darktheme: {
+          "primary": "#161618",
+          "secondary": "#4338ca",
+          "accent": "#c8102e",
+          "neutral": "#ffffff",
+          "base-100": "#f6f6f6",
+          "info": "#0000ff",
+          "success": "#00ff00",
+          "warning": "#00ff00",
+          "error": "#ff0000",
+        }
+      }
+    ]
   },
   plugins: [require("daisyui")]
 };
