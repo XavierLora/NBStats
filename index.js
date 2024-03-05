6d0c196245e93cb0e8a21ab90287793ff51d10a9
Xavier Lora <XavierLora0727@gmail.com>
2024-03-05 18:11:02 -0500

    Initial _site subtree commit

--- index.js
+++ index.js
@@ -1,33 +1,70 @@
-function getNBAScoreboard() {
+let upcomingGamesData;
+let topPlayersData;
+let topAthletesData = [];
+
+
+async function getUpcomingGames() {
     const apiUrl = 'https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard';
   
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('GET', apiUrl, true);
-      xhr.onload = function () {
-        if (xhr.status === 200) {
-          resolve(JSON.parse(xhr.responseText));
-        } else {
-          reject('Error fetching NBA scoreboard data');
+    return new Promise(async (resolve, reject) => {
+      try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('Error fetching NBA scoreboard data');
         }
-      };
-      xhr.onerror = function () {
-        reject('Error fetching NBA scoreboard data');
-      };
-      xhr.send();
+        upcomingGamesData = await response.json();
+        resolve(upcomingGamesData);
+    } catch (error) {
+        reject(new Error('Error fetching NBA scoreboard data'));
+    }
     });
   }
 
-getNBAScoreboard().then(data => {
-    console.log('NBA Scoreboard Data:', data);
-    console.log(data.events[0].competitions[0].competitors[0].team.displayName);
-    for(var i = 0; i<data.events.length; i++){
-        var obj = data.events[i];
-        displayUpcomingGames(obj);
+async function getTopPlayers(){
+  const apiUrl = 'http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons/2024/types/2/leaders';
+
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error('Error fetching top players data');
+      }
+      topPlayersData = await response.json();
+      // Assuming obj.categories[0].leaders[0].athlete.$ref is the URL of the athlete's data
+      for(let i = 0; i<3; i++){
+        const athleteUrl = topPlayersData.categories[0].leaders[i].athlete.$ref;
+        const athleteResponse = await fetch(athleteUrl);
+        if (!athleteResponse.ok) {
+          throw new Error('Error fetching athlete data');
+        }
+        const athleteData = await athleteResponse.json();
+        topAthletesData.push(athleteData);
+      }
+      
+      resolve({ topPlayersData, topAthletesData});
+    } catch (error) {
+      reject(new Error('Error fetching top players data'));
     }
-}).catch(error => {
-    console.error('Error:', error);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', async function() {
+  try {
+    const [upcomingGames, topPlayers] = await Promise.all([getUpcomingGames(), getTopPlayers()]);
+    console.log('Top Players Data:', topPlayers);
+      for (let i = 0; i < upcomingGamesData.events.length; i++) {
+          const obj = upcomingGamesData.events[i];
+          displayUpcomingGames(obj);
+      }
+      for (let i = 0; i < topAthletesData.length; i++) {
+        const obj = topAthletesData[i];
+        displayTopPlayers(obj);
+    }
+  } catch (error) {
+      console.error('Error:', error);
+  }
 });
+
 
 const upcomingGamesMachine = (obj) => {
   var team1 = obj.competitions[0].competitors[0].team;
@@ -35,26 +72,78 @@ const upcomingGamesMachine = (obj) => {
   var gameTeams = obj.shortName;
   var record1 = obj.competitions[0].competitors[0].records[0].summary;
   var record2 = obj.competitions[0].competitors[1].records[0].summary;
-  var date = obj.competitions[0].status.type.detail;
+  var date = obj.competitions[0].status.type.shortDetail;
   const makeGame = 
-  `<div class="flex gap-4">
-  <div class="avatar">
-    <div class="w-14 rounded-xl">
-      <img src="${team1.logo}" />
+    `<div class="flex gap-4">
+    <div class="avatar">
+      <div class="w-14 rounded-xl">
+        <img src="${team1.logo}" />
+      </div>
+    </div>
+    <h2 class="card-title">${gameTeams}</h2>
+    <div class="avatar">
+      <div class="w-14 rounded-xl">
+        <img src="${team2.logo}" />
+      </div>
     </div>
   </div>
-  <h2 class="card-title">${gameTeams}</h2>
-  <div class="avatar">
-    <div class="w-14 rounded-xl">
-      <img src="${team2.logo}" />
-    </div>
-  </div>
-</div>
-<p>${date}</p>`;
+  <p>${date}</p>`;
 
   return makeGame;
 }
 
+const topPlayersMachine = (obj) => {
+  var name = obj.fullName;
+  var img = obj.headshot.href;
+  var pts;
+  for(var i=0; i<topPlayersData.categories[0].leaders.length; i++){
+    if(topPlayersData.categories[0].leaders[i].athlete.$ref == obj.$ref){
+      var pts = topPlayersData.categories[0].leaders[i].displayValue;
+    }
+  }
+  const makePlayers = 
+    `<div class="collapse">
+    <input type="radio" name="my-accordion-1"/> 
+    <div class="collapse-title text-l font-medium">
+        <div class="playerList">
+            <div class="avatar">
+                <div class="w-14 rounded-full">
+                  <img src="${img}" />
+                </div>
+            </div>
+            <p>${name}</p>
+            <p class="cardPts">${pts}</p>
+        </div>
+        <div class="divider"></div> 
+    </div>
+    <div class="collapse-content text-center"> 
+        <div class="stats shadow w-11/12">
+            <div class="stat place-items-center">
+            <div class="stat-title text-base">REB</div>
+            <div class="stat-value text-xl">8.8</div>
+            <div class="stat-desc text-sm">18th</div>
+            </div>
+            <div class="stat place-items-center">
+            <div class="stat-title text-base">AST</div>
+            <div class="stat-value text-xl">9.5</div>
+            <div class="stat-desc text-sm">3rd</div>
+            </div>
+            <div class="stat place-items-center">
+            <div class="stat-title text-base">FG%</div>
+            <div class="stat-value text-xl">49.2</div>
+            <div class="stat-desc text-sm">54th</div>
+            </div>
+        </div>
+    </div>
+  </div>`
+
+  return makePlayers;
+}
+
+function displayTopPlayers(obj){
+  let parentNode = document.getElementById('topPlayersContainer');
+    parentNode.insertAdjacentHTML('beforeend', topPlayersMachine(obj));
+}
 
 function displayUpcomingGames(obj){
   let parentNode = document.getElementById('upcomingGamesCardContainer');
