16fd9faf7538e157427b6a11ccbe50aa9523ccdd
Xavier Lora <XavierLora0727@gmail.com>
2024-03-06 21:38:47 -0500

    Fixing More UI issues for Mobile

--- _site/index.js
+++ _site/index.js
@@ -120,7 +120,7 @@ const upcomingGamesMachine = (obj) => {
   var team1 = obj.competitions[0].competitors[0].team;
   var team2 = obj.competitions[0].competitors[1].team;
   var gameTeams = obj.shortName;
-  var isGameActive = obj.status.type.state == "in";
+  var isGameActive = obj.status.type.state == "in" || obj.status.type.state == "post";
   var score1 = obj.competitions[0].competitors[0].score;
   var score2 = obj.competitions[0].competitors[1].score;
   console.log(isGameActive);
@@ -128,9 +128,9 @@ const upcomingGamesMachine = (obj) => {
   var record2 = obj.competitions[0].competitors[1].records[0].summary;
   var date = obj.competitions[0].status.type.shortDetail;
   const makeGame = `
-    <div class="collapse">
+    <div class="collapse w-11/12">
       <input type="radio" name="my-accordion-1"/>
-      <div class="collapse-title text-l font-medium">
+      <div class="collapse-title text-l font-medium flex flex-col justify-center">
         <div class="flex gap-4 justify-center text-center">
           <div class="avatar">
             <div class="w-14 rounded-xl">
@@ -145,17 +145,17 @@ const upcomingGamesMachine = (obj) => {
           </div>
         </div>
         <p class="text-center">${date}</p>
-        <div class="divider pr-6"></div>
+        <div class="divider w-full"></div>
       </div>
       <div class="collapse-content text-center">
         <div class="stats shadow w-10/12">
           ${isGameActive ? // Check if game is active
             `<div class="stat place-items-center">
-              <div class="stat-title text-base">Live Score</div>
+              <div class="stat-title text-base">Score</div>
               <div class="stat-value text-xl">${score1}</div>
             </div>
             <div class="stat place-items-center">
-              <div class="stat-title text-base">Live Score</div>
+              <div class="stat-title text-base">Score</div>
               <div class="stat-value text-xl">${score2}</div>
             </div>` :
             `<div class="stat place-items-center">
