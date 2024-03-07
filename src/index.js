4afaee61aafc385164a3201f1e4066dbe4a5c228
Xavier Lora <XavierLora0727@gmail.com>
2024-03-06 20:51:49 -0500

    Updates to functionality for live Games on the home Page!

--- src/index.js
+++ src/index.js
@@ -120,42 +120,56 @@ const upcomingGamesMachine = (obj) => {
   var team1 = obj.competitions[0].competitors[0].team;
   var team2 = obj.competitions[0].competitors[1].team;
   var gameTeams = obj.shortName;
+  var isGameActive = obj.status.type.state == "in";
+  var score1 = obj.competitions[0].competitors[0].score;
+  var score2 = obj.competitions[0].competitors[1].score;
+  console.log(isGameActive);
   var record1 = obj.competitions[0].competitors[0].records[0].summary;
   var record2 = obj.competitions[0].competitors[1].records[0].summary;
   var date = obj.competitions[0].status.type.shortDetail;
-  const makeGame = 
-    `<div class="collapse">
+  const makeGame = `
+    <div class="collapse">
       <input type="radio" name="my-accordion-1"/>
       <div class="collapse-title text-l font-medium">
-        <div class="flex gap-4 justify-center">
-        <div class="avatar">
-          <div class="w-14 rounded-xl">
-            <img src="${team1.logo}" />
+        <div class="flex gap-4 justify-center text-center">
+          <div class="avatar">
+            <div class="w-14 rounded-xl">
+              <img src="${team1.logo}" />
+            </div>
+          </div>
+          <h2 class="card-title">${gameTeams}</h2>
+          <div class="avatar">
+            <div class="w-14 rounded-xl">
+              <img src="${team2.logo}" />
+            </div>
           </div>
         </div>
-        <h2 class="card-title">${gameTeams}</h2>
-        <div class="avatar">
-          <div class="w-14 rounded-xl">
-            <img src="${team2.logo}" />
-          </div>
-        </div>
-    </div>
-    <p class="text-center">${date}</p>
-    <div class="divider"></div>
-    </div>
-    <div class="collapse-content text-center">
-      <div class="stats shadow w-11/12">
-          <div class="stat place-items-center">
-            <div class="stat-title text-base">Record</div>
-            <div class="stat-value text-xl">${record1}</div>
-          </div>
-          <div class="stat place-items-center">
-            <div class="stat-title text-base">Record</div>
-            <div class="stat-value text-xl">${record2}</div>
-          </div>
+        <p class="text-center">${date}</p>
+        <div class="divider"></div>
       </div>
-    </div>
-  </div>`;
+      <div class="collapse-content text-center">
+        <div class="stats shadow" id="playerStatWidth">
+          ${isGameActive ? // Check if game is active
+            `<div class="stat place-items-center">
+              <div class="stat-title text-base">Score</div>
+              <div class="stat-value text-xl">${score1}</div>
+            </div>
+            <div class="stat place-items-center">
+              <div class="stat-title text-base">Score</div>
+              <div class="stat-value text-xl">${score2}</div>
+            </div>` :
+            `<div class="stat place-items-center">
+              <div class="stat-title text-base">Record</div>
+              <div class="stat-value text-xl">${record1}</div>
+            </div>
+            <div class="stat place-items-center">
+              <div class="stat-title text-base">Record</div>
+              <div class="stat-value text-xl">${record2}</div>
+            </div>`
+          }
+        </div>
+      </div>
+    </div>`;
 
   return makeGame;
 }
@@ -238,13 +252,15 @@ const topTeamsMachine = (obj) => {
   var objTeamLink = obj.team.$ref;
   var secureTeamLink = objTeamLink.replace('http:', 'https:');
   var teamName;
+  var teamCity;
   var teamImg;
 
   var i = 0;
   
   while (typeof teamName === 'undefined') {
     if ((topTeamsData[i].$ref).replace('http:', 'https:') === secureTeamLink) {
-            teamName = topTeamsData[i].displayName;
+            teamName = topTeamsData[i].name;
+            teamCity = topTeamsData[i].abbreviation;
             var temp = topTeamsData[i].logos[0].href;
             var teamImg = temp.replace('http:', 'https:');
         }
@@ -260,19 +276,19 @@ const topTeamsMachine = (obj) => {
                   <img src="${teamImg}" />
                 </div>
             </div>
-            <p class="teamName">${teamName}</p>
+            <p class="teamName">${teamCity} ${teamName}</p>
             <p class="cardPts">${wins}-${loses}</p>
         </div>
         <div class="divider"></div> 
     </div>
     <div class="collapse-content text-center"> 
-    <div class="stats shadow w-11/12">
+    <div class="stats shadow" id="playerStatWidth">
     <div class="stat place-items-center">
       <div class="stat-title text-base">RANK</div>
-      <div class="stat-value text-base">${place}</div>
+      <div class="stat-value text-xl">${place}</div>
     </div>
     <div class="stat place-items-center">
-      <div class="stat-title text-sm">Strength of Schedule</div>
+      <div class="stat-title text-base">Strength of Sch.</div>
       <div class="stat-value text-xl">${sos}</div>
     </div>
 </div>
