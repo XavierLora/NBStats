bb7c97a14d69ad330b104f26affe77170a66e58f
Xavier Lora <XavierLora0727@gmail.com>
2024-03-10 09:51:03 -0400

    Updates to Live and Post Game Stats

--- live/live.js
+++ live/live.js
@@ -85,6 +85,38 @@ const liveGamesMachine = (obj) => {
     var gameQtr2Team2 = obj.competitions[0].competitors[1].linescores[1].value;
     var gameQtr3Team2 = obj.competitions[0].competitors[1].linescores[2].value;
     var gameQtr4Team2 = obj.competitions[0].competitors[1].linescores[3].value;
+
+    var team1PtsLeaderImg = obj.competitions[0].competitors[0].leaders[0].leaders[0].athlete.headshot;
+    var team1PtsLeader = obj.competitions[0].competitors[0].leaders[0].leaders[0].athlete.shortName;
+    var team1PtsLeaderValue = obj.competitions[0].competitors[0].leaders[0].leaders[0].value;
+    var team1PtsLeaderPos = obj.competitions[0].competitors[0].leaders[0].leaders[0].athlete.position.abbreviation;
+
+
+    var team1AstLeaderImg = obj.competitions[0].competitors[0].leaders[1].leaders[0].athlete.headshot;
+    var team1AstLeader = obj.competitions[0].competitors[0].leaders[1].leaders[0].athlete.shortName;
+    var team1AstLeaderValue = obj.competitions[0].competitors[0].leaders[1].leaders[0].value;
+    var team1AstLeaderPos = obj.competitions[0].competitors[0].leaders[1].leaders[0].athlete.position.abbreviation;
+
+    var team1RebLeaderImg = obj.competitions[0].competitors[0].leaders[2].leaders[0].athlete.headshot;
+    var team1RebLeader = obj.competitions[0].competitors[0].leaders[2].leaders[0].athlete.shortName;
+    var team1RebLeaderValue = obj.competitions[0].competitors[0].leaders[2].leaders[0].value;
+    var team1RebLeaderPos = obj.competitions[0].competitors[0].leaders[2].leaders[0].athlete.position.abbreviation;
+
+    var team2PtsLeaderImg = obj.competitions[0].competitors[1].leaders[0].leaders[0].athlete.headshot;
+    var team2PtsLeader = obj.competitions[0].competitors[1].leaders[0].leaders[0].athlete.shortName;
+    var team2PtsLeaderValue = obj.competitions[0].competitors[1].leaders[0].leaders[0].value;
+    var team2PtsLeaderPos = obj.competitions[0].competitors[1].leaders[0].leaders[0].athlete.position.abbreviation;
+
+
+    var team2AstLeaderImg = obj.competitions[0].competitors[1].leaders[1].leaders[0].athlete.headshot;
+    var team2AstLeader = obj.competitions[0].competitors[1].leaders[1].leaders[0].athlete.shortName;
+    var team2AstLeaderValue = obj.competitions[0].competitors[1].leaders[1].leaders[0].value;
+    var team2AstLeaderPos = obj.competitions[0].competitors[1].leaders[1].leaders[0].athlete.position.abbreviation;
+
+    var team2RebLeaderImg = obj.competitions[0].competitors[1].leaders[2].leaders[0].athlete.headshot;
+    var team2RebLeader = obj.competitions[0].competitors[1].leaders[2].leaders[0].athlete.shortName;
+    var team2RebLeaderValue = obj.competitions[0].competitors[1].leaders[2].leaders[0].value;
+    var team2RebLeaderPos = obj.competitions[0].competitors[1].leaders[2].leaders[0].athlete.position.abbreviation;
     
     console.log(isGameActive);
     var record1 = obj.competitions[0].competitors[0].records[0].summary;
@@ -150,7 +182,7 @@ const liveGamesMachine = (obj) => {
       <div class="collapse-content text-center">
         <div class="collapse-title text-l font-medium flex flex-col justify-center">
         ${isGamePre ? // Check if game is pre
-        `<p class="text-center" id="liveStatsTitle">Team Stats</p>
+        `<p class="text-center" id="liveStatsTitle">Team Leaders</p>
         <div class="stats shadow w-full bg-neutral">
           <div role="tablist" class="tabs tabs-bordered justify-center">
             <input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="${name2}" />
@@ -394,7 +426,7 @@ const liveGamesMachine = (obj) => {
                 </table>
                 </div>
               </div>
-              <p class="text-center" id="liveStatsTitle">Team Stats</p>
+              <p class="text-center" id="liveStatsTitle">Team Leaders</p>
         <div class="stats shadow w-full bg-neutral">
           <div role="tablist" class="tabs tabs-bordered justify-center">
             <input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="${name2}" />
@@ -405,32 +437,51 @@ const liveGamesMachine = (obj) => {
               <thead>
                 <tr>
                   <th></th>
+                  <th>Player</th>
                   <th>Total</th>
-                  <th>AVG</th>
-                  <th>Rank</th>
+                  <th>POS</th>
                 </tr>
               </thead>
               <tbody>
                 <!-- row 1 -->
-                <tr>
+                <!-- row 1 -->
+                  <tr>
                   <th>${prePtsStatName}</th>
-                  <td>${prePtsStatTotal2}</td>
-                  <td>${prePtsStatAvg2}</td>
-                  <td>${prePtsStatRank2}</td>
+                  <td id="playerNameGamesContainer">
+                    <div class="avatar">
+                      <div class="w-10 rounded-full">
+                        <img src="${team2PtsLeaderImg}" />
+                      </div>
+                    </div>
+                    ${team2PtsLeader}</td>
+                  <td>${team2PtsLeaderValue}</td>
+                  <td>${team2PtsLeaderPos}</td>
                 </tr>
                 <!-- row 2 -->
                 <tr>
-                <th>${preAstStatName}</th>
-                <td>${preAstStatTotal2}</td>
-                <td>${preAstStatAvg2}</td>
-                <td>${preAstStatRank2}</td>
+                  <th>${preAstStatName}</th>
+                  <td id="playerNameGamesContainer">
+                    <div class="avatar">
+                      <div class="w-10 rounded-full">
+                        <img src="${team2AstLeaderImg}" />
+                      </div>
+                    </div>
+                    ${team2AstLeader}</td>
+                  <td>${team2AstLeaderValue}</td>
+                  <td>${team2AstLeaderPos}</td>
                 </tr>
                 <!-- row 3 -->
                 <tr>
-                <th>${preRebStatName}</th>
-                <td>${preRebStatTotal2}</td>
-                <td>${preRebStatAvg2}</td>
-                <td>${preRebStatRank2}</td>
+                  <th>${preRebStatName}</th>
+                  <td id="playerNameGamesContainer">
+                    <div class="avatar">
+                      <div class="w-10 rounded-full">
+                        <img src="${team2RebLeaderImg}" />
+                      </div>
+                    </div>
+                    ${team2RebLeader}</td>
+                  <td>${team2RebLeaderValue}</td>
+                  <td>${team2RebLeaderPos}</td>
                 </tr>
               </tbody>
             </table>
@@ -444,32 +495,50 @@ const liveGamesMachine = (obj) => {
                 <thead>
                   <tr>
                     <th></th>
+                    <th>Name</th>
                     <th>Total</th>
-                    <th>AVG</th>
-                    <th>Rank</th>
+                    <th>POS</th>
                   </tr>
                 </thead>
                 <tbody>
                   <!-- row 1 -->
                   <tr>
                   <th>${prePtsStatName}</th>
-                  <td>${prePtsStatTotal1}</td>
-                  <td>${prePtsStatAvg1}</td>
-                  <td>${prePtsStatRank1}</td>
+                  <td id="playerNameGamesContainer">
+                    <div class="avatar">
+                      <div class="w-10 rounded-full">
+                        <img src="${team1PtsLeaderImg}" />
+                      </div>
+                    </div>
+                    ${team1PtsLeader}</td>
+                  <td>${team1PtsLeaderValue}</td>
+                  <td>${team1PtsLeaderPos}</td>
                 </tr>
                 <!-- row 2 -->
                 <tr>
-                <th>${preAstStatName}</th>
-                <td>${preAstStatTotal1}</td>
-                <td>${preAstStatAvg1}</td>
-                <td>${preAstStatRank1}</td>
+                  <th>${preAstStatName}</th>
+                  <td id="playerNameGamesContainer">
+                    <div class="avatar">
+                      <div class="w-10 rounded-full">
+                        <img src="${team1AstLeaderImg}" />
+                      </div>
+                    </div>
+                    ${team1AstLeader}</td>
+                  <td>${team1AstLeaderValue}</td>
+                  <td>${team1AstLeaderPos}</td>
                 </tr>
                 <!-- row 3 -->
                 <tr>
-                <th>${preRebStatName}</th>
-                <td>${preRebStatTotal1}</td>
-                <td>${preRebStatAvg1}</td>
-                <td>${preRebStatRank1}</td>
+                  <th>${preRebStatName}</th>
+                  <td id="playerNameGamesContainer">
+                    <div class="avatar">
+                      <div class="w-10 rounded-full">
+                        <img src="${team1RebLeaderImg}" />
+                      </div>
+                    </div>
+                    ${team1RebLeader}</td>
+                  <td>${team1RebLeaderValue}</td>
+                  <td>${team1RebLeaderPos}</td>
                 </tr>
                 </tbody>
               </table>
