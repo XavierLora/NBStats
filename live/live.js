3b2556c9cea8b72a28fbdff8d5b78a43dcae52f1
Xavier Lora <XavierLora0727@gmail.com>
2024-03-18 10:44:49 -0400

    Changing Size of X

--- live/live.js
+++ live/live.js
@@ -631,7 +631,11 @@ const liveGamesMachine = (obj, DataTeam1, DataTeam2) => {
         <div class="modal" role="dialog">
           <div class="modal-box">
           <div class="modal-action">
-            <label for="my_modal_${gameID}" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">X</label>
+            <label for="my_modal_${gameID}" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-10">
+            <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clip-rule="evenodd" />
+          </svg>          
+            </label>
           </div>
           <div class="flex gap-4 justify-around text-center">
               <h2 class="card-title text-2xl">${score2}</h2>
@@ -895,7 +899,11 @@ const liveGamesMachine = (obj, DataTeam1, DataTeam2) => {
           <div class="modal" role="dialog">
             <div class="modal-box">
             <div class="modal-action">
-              <label for="my_modal_${gameID}" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">X</label>
+              <label for="my_modal_${gameID}" class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-10">
+              <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z" clip-rule="evenodd" />
+            </svg>            
+              </label>
             </div>
             <div class="flex gap-4 justify-around text-center">
                 <h2 class="card-title text-2xl">${score2}</h2>
